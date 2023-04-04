1a0bea411a048bcee231828f1e2882b9bf6470d0
notsquirrelcoding <carros0508@rpsmn.org>
2023-04-04 14:34:17 -0500

    first commitfwe

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def main():
     options = json.load(open("options.json"))
     sets = options["sets"]
     menu = consolemenu.SelectionMenu.get_selection(sets)
-    set = sets[menu]
-    for key, value in options[set].items():
-        keyboard.add_abbreviation(key, value)
-    keyboard.wait()
+
+    if menu == len(sets):
+        print("add")
+    else:
+        set = sets[menu]
+        for key, value in options[set].items():
+            keyboard.add_abbreviation(key, value)
+        keyboard.wait()
 
 
 if __name__ == "__main__":
